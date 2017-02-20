e65d63bb9d7e2b56d6b120f37f9d0e796362dc45
Richard Wright <rbwrightjr91@gmail.com>
2017-02-20 15:51:54 -0800

    new lab and adding source code

---
+++ Systems Programming in C/Lab 3/Lab3.c
@@ -0,0 +1,31 @@
+/*                 */
+/* Richard Wright  */
+/* Section 1       */
+/* Lab 3           */
+/* Date: 02/28/17  */
+/*                 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+	int age = 25;
+	double average = 246.7531986;
+
+	printf("\nRichard Wright. Lab 3.\n\n");
+	
+	printf("The dog ran quickly.\n\n");
+	
+	printf("Joe is %i years old.\n\n", age);
+	
+	printf("\'So many books, so little time.\'\n- Frank Zappa\n\n");
+	
+	printf("The average = %.1f\n\n", average);
+
+	printf("The average = %.3f\n\n", average);	
+	
+	printf("The average = %.2e\n\n", average);
+
+}
